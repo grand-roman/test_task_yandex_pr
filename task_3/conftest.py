11deaaaf61f71d037b34c12c1c04ad20461eea1c
Roman <31464179+grand-roman@users.noreply.github.com>
2022-08-12 16:55:26 +0700

    add tests

--- task_3/conftest.py
+++ task_3/conftest.py
@@ -1,3 +1,5 @@
+import sys
+from io import StringIO
 from pathlib import Path
 
 import pytest
@@ -13,12 +15,47 @@ def user_code():
         return f.read()
 
 
-@pytest.fixture(scope='module')
-def output():
-    return ' '.join([
-        'Время выполнения функции: 1.0 с. 2',
-        'Время выполнения функции: 0.0 с. 2',
-        'Время выполнения функции: 1.0 с. 4',
-        'Время выполнения функции: 0.0 с. 4',
-        'Время выполнения функции: 0.0 с. 4'
-    ])
+@pytest.fixture(scope='session')
+def get_precode_stdout():
+    _stdout = sys.stdout
+    sys.stdout = _stringio = StringIO()
+    import precode
+    output = _stringio.getvalue()
+    sys.stdout = _stdout
+    return (precode, output)
+
+
+@pytest.fixture
+def precode(get_precode_stdout):
+    return get_precode_stdout[0]
+
+
+@pytest.fixture
+def student_output(get_precode_stdout):
+    return get_precode_stdout[1]
+
+
+@pytest.fixture
+def author_output(output_param):
+    import author
+    return output_param.readouterr().out
+
+
+@pytest.fixture
+def missing_variables(precode):
+    expected_variables = ('time_check', 'cache_args')
+    missing_variables = []
+    for var in expected_variables:
+        if var not in precode.__dict__:
+            missing_variables.append(var)
+    return missing_variables
+
+
+@pytest.fixture
+def missing_imports(precode):
+    expected_imports = ('time',)
+    missing_imp = []
+    for module_name in expected_imports:
+        if module_name not in precode.__dict__:
+            missing_imp.append(module_name)
+    return missing_imp
--- task_3/test.py
+++ task_3/test.py
@@ -1,24 +1,6 @@
 import re
 
 
-TEXT_TEMPLATE = [
-    'Время выполнения функции: 1.0 с. 2',
-    'Время выполнения функции: 0.0 с. 2',
-    'Время выполнения функции: 1.0 с. 4',
-    'Время выполнения функции: 0.0 с. 4',
-    'Время выполнения функции: 0.0 с. 4'
-]
-
-
-def test_output(output):
-    assert ' '.join(
-        TEXT_TEMPLATE
-    ) in output, (
-        'Проверьте правильность вывода функции "long_heavy". '
-        'Возможно вы не использовали "wraps" в декорирующих ее функциях'
-    )
-
-
 def test_cache_dict_exist(user_code):
     assert re.compile(
         r'\w+ = ({}|dict\(\))'
@@ -33,3 +15,41 @@ def test_cache_return(user_code):
     ).findall(
         user_code
     ), 'Проверьте, возвращается ли кэш'
+
+
+def test_precode_variables_exist(missing_variables):
+    assert not missing_variables, (
+        f'Пожалуйста, используйте переменные {", ".join(missing_variables)} '
+        'из прекода.'
+    )
+
+
+def test_expected_imports_exist(missing_imports):
+    assert not missing_imports, (
+        f'Пожалуйста, импортируйте модуль {", ".join(missing_imports)}.'
+    )
+
+
+def test_stdout(author_output, student_output):
+    if student_output:
+        student_lines = student_output.strip().split('\n')
+        expected_lines = author_output.strip().split('\n')
+        if len(expected_lines) == len(student_lines):
+            for line_num, (
+                expected_line, student_line
+            ) in enumerate(
+                zip(expected_lines, student_lines),
+                1
+            ):
+                assert expected_line == student_line, (
+                    'Результат не соответствует ожидаемому.\n'
+                    f'Проверьте {line_num} строку результата '
+                    'выполнения кода. Она должная выглядеть '
+                    'следующим образом:\n'
+                    f'{expected_line}'
+                )
+    assert author_output == student_output, (
+        'Результат не соответствует ожидаемому:\n'
+        f'Ваш вывод:\n {student_output}\n'
+        f'Ожидаемый вывод:\n {author_output}'
+    )
